4a237e1a01f4db4e16f46d840949620b29c34372
Valkyrion3 <126981442+Valkyrion3@users.noreply.github.com>
2023-03-04 21:09:37 -0600

    Ejercicio 3
    Pedir datos por teclado

---
+++ ex_03_03.py
@@ -0,0 +1,9 @@
+def main():
+	print("Ejercicio 3: Multiplicación")
+	xh = input("Enter hours: ")
+	xr = input("Enter rate: ")
+	xp = xh * xr
+	print("Pay:", xp)
+	
+if __name__ == "__main__":
+	main():

0e3959aa501f346bb6830f0fb250e4a8937a46e2
Valkyrion <valk.neo@gmail.com>
2023-03-04 21:21:48 -0600

    Corrección de archivos

--- ex_02_03.py
+++ ex_02_03.py
@@ -1,9 +1,9 @@
 def main():
 	print("Ejercicio 3: Multiplicación")
-	xh = input("Enter hours: ")
-	xr = input("Enter rate: ")
+	xh = int(input("Enter hours: "))
+	xr = int(input("Enter rate: "))
 	xp = xh * xr
 	print("Pay:", xp)
 	
 if __name__ == "__main__":
-	main():
+	main()

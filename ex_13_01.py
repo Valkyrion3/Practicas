6a9d577b08a1c13d5f32a4f84d51e21bc38e57bd
Valkyrion <valk.neo@gmail.com>
2023-03-07 01:41:39 -0600

    XML

--- ex_13_01.py
+++ ex_13_01.py
@@ -1,32 +1,42 @@
-import urllib.request, urllib.parse, urllib.error
-import json
+import xml.etree.ElementTree as TE
 
 def main():
-    print("API")
-    serviceurl = "http://py4e-data.dr-chuck.net/geojson?"
-    while True:
-        address = input("Enter location: ")
-        if len(address)<1:
-            break
-        url = serviceurl + urllib.parse.urlencode({"address":address})
-        print("Retrieving",url)
-        uh = urllib.request.urlopen(url)
-        data = uh.read().decode()
-        print("Retrieved",len(data),"characters")
-        try:
-            js = json.loads(data)
-        except:
-            js = None
-        if not js or "status" not in js or js["status"] != "OK":
-            print("===Failure to retrieve===")
-            print(data)
-            continue
-        print(json.dumps(js,indent=4))
-        lat = js["results"][0]["geometry"]["location"]["lat"]
-        lng = js["results"][0]["geometry"]["location"]["lng"]
-        print("Lat:",lat,", Lng",lng)
-        location = js["results"][0]["formatted_address"]
-        print("Location:",location)
-    
+    print("---XML---")
+    data = '''
+    <person>
+        <name>Valkyrion</name>
+        <phone type="intl">
+            333999
+        </phone>
+        <email hide="yes"/>
+    </person>
+    '''
+    tree = TE.fromstring(data)
+    print("Name:",tree.find("name").text)
+    print("Attr:",tree.find("email").get("hide"))
+    print("---XML List---")
+    input = '''
+    <stuff>
+        <users>
+            <user x="3">
+                <id>001</id>
+                <name>Valkyrion</name>
+            </user>
+            <user x="7">
+                <id>005</id>
+                <name>Monse</name>
+            </user>
+        </users>
+    </stuff>'''
+
+    stuff = TE.fromstring(input)
+    lst = stuff.findall('users/user')
+    print('User count:', len(lst))
+
+    for item in lst:
+        print('Name', item.find('name').text)
+        print('Id', item.find('id').text)
+        print('Attribute', item.get('x'))
+
 if __name__ == "__main__":
     main()
